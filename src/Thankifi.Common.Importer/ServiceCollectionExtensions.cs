7a2ef95f58f2d8495fd5073df44b978548027710
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-08-10 14:11:56 +0300

    feat: add DI

---
+++ src/Thankifi.Common.Importer/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Thankifi.Common.Importer.Options;
+
+namespace Thankifi.Common.Importer
+{
+    /// <summary>
+    /// Service collection extensions.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds filters and related services to the container.
+        /// </summary>
+        /// <param name="services"><see cref="IServiceCollection"/>.</param>
+        /// <param name="options"></param>
+        /// <returns><see cref="IServiceCollection"/></returns>
+        public static IServiceCollection AddImporter(this IServiceCollection services, Action<ImportOptions>? options)
+        {
+            var importerConfig = services.AddOptions<ImportOptions>().BindConfiguration(ImportOptions.Section);
+            
+            if (options is not null)
+            {
+                importerConfig.Configure(options);
+            }
+            
+            services.AddHttpClient<IImporter, Importer>();
+
+
+            return services;
+        }
+    }
+}
