b06914e1904e680fb20045b728e98cb413a29425
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-08-10 14:17:22 +0300

    feat: moved abastractions to their own project

--- src/Thankifi.Common.Importer/Importer.cs
+++ src/Thankifi.Common.Importer/Importer.cs
@@ -3,7 +3,6 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Net.Mime;
-using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
