9703177cf5e3240ca6b69d43658ac83d47bbadba
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-08-10 14:07:12 +0300

    feat: add ImportOptions

---
+++ src/Thankifi.Common.Importer/Options/ImportOptions.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Thankifi.Common.Importer.Options
+{
+    public class ImportOptions
+    {
+        public static readonly string Section = "Import"; 
+        public ImportOptions(string source)
+        {
+            Source = new Uri(source) ?? throw new ArgumentException("Dataset source is not a valid url");
+        }
+
+        public Uri Source { get; }
+    }
+    
+}
