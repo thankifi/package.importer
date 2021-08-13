2877967b1ee0f9d0aa9c25ee157590b9027daa91
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-08-13 17:35:46 +0300

    feat: add default constructor for ImportOptions

--- src/Thankifi.Common.Importer.Abstractions/Options/ImportOptions.cs
+++ src/Thankifi.Common.Importer.Abstractions/Options/ImportOptions.cs
@@ -4,13 +4,19 @@ namespace Thankifi.Common.Importer.Abstractions.Options
 {
     public class ImportOptions
     {
-        public static readonly string Section = "Import"; 
+        public static readonly string Section = "Import";
+
+        public ImportOptions()
+        {
+            Source = new Uri("https://github.com/thankifi/dataset/blob/main/src/");
+        }
+        
         public ImportOptions(string source)
         {
             Source = new Uri(source) ?? throw new ArgumentException("Dataset source is not a valid url");
         }
 
-        public Uri Source { get; }
+        public Uri Source { get; set; }
     }
     
 }
